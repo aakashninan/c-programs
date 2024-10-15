58aa6f74ecb6be3e64ddcae19664078d371810f2
aakashninan <aakashninangikku@gmail.com>
2024-10-15 10:11:14 +0530

    Check whether a year is a leap year

--- main.c
+++ main.c
@@ -1,13 +1,20 @@
 #include <stdio.h>
-int main () {
-    int p,r,t,si;
-    printf("Enter the Principal Amount :" );
-    scanf("%d",&p);
-    printf("Enter the Rate :" );
-    scanf("%d",&r);
-    printf("Enter the Time Period  :" );
-    scanf("%d",&t);
-    si=(p*r*t)/100;
-    printf("The Simple Interest is : %d",si);
+int main() {
+    int year;
+    printf("Enter the year : ");
+    scanf("%d", &year);
+    if (year%400==0) {
+        printf("%d is a leap year",year);
+    }
+    else if (year%100==0 && year%4==0) {
+        printf("%d is  not a leap year",year);
+    }
+    else if (year%4==0) {
+        printf("%d is a  leap year",year);
+    }
+    else {
+        {
+            printf("%d is not a leap year",year);
+        }}
     return 0;
 }
